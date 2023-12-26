126ba313f847c4af3d9b600cef5457e1ce5a2e1c
Jan Skowron <jasiusko1@gmail.com>
2023-12-26 18:35:26 +0100

    final structure

--- app.py
+++ app.py
@@ -12,14 +12,38 @@ clips_file.reset()
 clips_file.run()
 clips_file.run()
 # Pobieranie wyników
-for fact in clips_file.facts():
-    print(fact)
+def actualquestion():
+    for fact in clips_file.facts():
+        if list(fact)[0]=="question":
+            return list(fact)
+
+    # print(fact)
+def printallfacts():
+    for fact in clips_file.facts():
+        print(fact)
+def possibleanswers(fact):
+    possibleanswers=[]
+    fact=fact[2:]
+    for i in range(0,len(fact)):
+        if i%2==0:
+            possibleanswers.append(fact[i])
+    return possibleanswers
+        
 while True:
-    t=input("Press Enter to continue...")
+    fact=actualquestion()
+    print(fact[1])
+    print("Possible answers:")
+    for possibleanswer in possibleanswers(fact):
+        print(possibleanswer)
+    t=input("")
     if t=="":
         break
-    clips_file.assert_fact("actualanswer {t}")
+    clips_file.assert_string('(actualanswer "'+t+'")')
+    
     clips_file.run()
-# Zamykanie pliku
+    
+    printallfacts()
+
 clips_file.clear()
+
 
--- rules.clp
+++ rules.clp
@@ -1,18 +1,21 @@
 (deffacts poczatek
 
-    (question "What type od game do you want to play?" "A Strategy Game" "Dzem" "I want a Wargame" "Kolega")
+    (question "question" "What type od game do you want to play?" "A Strategy Game" "Dzem" "I want a Wargame" "Kolega")
     (actualquestion "What type od game do you want to play?")
     
     (question "Does theme matter to you?" "Yes" "No")
     
+    
 )
 
 (defrule mainrule
-    ?f<-(actualanswer ?answer)
-    (question ?question $?answers)
+    ?f<-(actualanswer ?question)
+    (actualanswer ?answer)
+    (question ?question $?answers1 ?answer ?nextquestion $?answers2)
     =>
     (retract ?f)
-    (assert (answer ?question $?answers))
+    (assert (answer ?question ?answer))
+    (assert (actualquestion ?nextquestion))
 
 )
 
